a5fed6aae0813ec1c56b04a30e72ea7b91664b9c
AsianJack <lucastanakinha100@hotmail.com>
2024-06-25 14:12:35 -0300

    Controle de Fluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(){
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
